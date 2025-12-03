8e82ccf19a0746931c5337603acbc05cf81107a1
aki366 <akihiro.okuda366@gmail.com>
2025-12-03 22:58:18 +0900

    add: Greetingクラスと実行手順をREADMEに追加

---
+++ java_swing/Greeting.java
@@ -0,0 +1,11 @@
+package java_swing;
+
+public class Greeting {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("arg = " + args[0]);
+        } else {
+            System.out.println("no args");
+        }
+    }
+}
---
+++ java_swing/README.md
@@ -0,0 +1,11 @@
+# Java
+
+## Javaのコンパイル
+```bash
+javac -d build *.java
+```
+
+## Javaの実行
+```bash
+java -cp build java_swing.Greeting world
+```
